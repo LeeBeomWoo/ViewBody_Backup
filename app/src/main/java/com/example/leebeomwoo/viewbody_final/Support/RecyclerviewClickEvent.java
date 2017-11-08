d06a129cd556d78709d70fd2cbce83a56ecc3566
LeeBeomWoo <vvaeru6@gmail.com>
2017-11-08 19:20:38 +0900

    complete remind design

--- app/src/main/java/com/example/leebeomwoo/viewbody_final/Support/RecyclerviewClickEvent.java
+++ app/src/main/java/com/example/leebeomwoo/viewbody_final/Support/RecyclerviewClickEvent.java
@@ -1,28 +1,39 @@
 package com.example.leebeomwoo.viewbody_final.Support;
 
+import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.ColorFilter;
+import android.graphics.Matrix;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
+import android.os.Handler;
 import android.support.annotation.IntRange;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.view.MotionEventCompat;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.leebeomwoo.viewbody_final.Item.ListDummyItem;
 import com.example.leebeomwoo.viewbody_final.R;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+import cn.gavinliu.android.lib.scale.ScaleRelativeLayout;
+import cn.gavinliu.android.lib.scale.helper.ScaleLayoutHelper;
 
 import static android.view.MotionEvent.INVALID_POINTER_ID;
 
@@ -30,18 +41,28 @@ import static android.view.MotionEvent.INVALID_POINTER_ID;
  * Created by LeeBeomWoo on 2017-06-21.
  */
 
-public class RecyclerviewClickEvent /**implements View.OnTouchListener*/ {
-    Context context;
-    Drawable drawable ;
-    private int mActivePointerId = INVALID_POINTER_ID;
-    public void Click(ListDummyItem ldItem, Context context){
+public class RecyclerviewClickEvent implements View.OnTouchListener {
+    private Context context;
+    private Drawable drawable ;
+    private int _xDelta, _yDelta, width, height;
+    float scaleWidth, scaleHeight;
+    private ImageView imgViewIcon;
+    private ListDummyItem ldItem;
+    boolean end;
+    Handler handler;
+    Matrix matrix;
+    public void Click(ListDummyItem ld_Item, Context context){
         this.context = context;
+        ldItem = ld_Item;
         //read your lovely variable
+        Log.d("Popup", ldItem.getLd_ImageUrl());
         Dialog dialog = new Dialog(context);
         dialog.setContentView(R.layout.fragment_detail);
         dialog.getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT);
+
+        ScaleRelativeLayout main = dialog.findViewById(R.id.detail_layout);
         TextView txtViewTitle = (TextView) dialog.findViewById(R.id.detile_Title);
-        ImageView imgViewIcon = (ImageView) dialog.findViewById(R.id.detile_Image);
+        imgViewIcon = (ImageView) dialog.findViewById(R.id.detile_Image);
         TextView video_title_1 = (TextView) dialog.findViewById(R.id.video_title_1);
         TextView video_title_2 = (TextView) dialog.findViewById(R.id.video_title_2);
         TextView video_title_3 = (TextView) dialog.findViewById(R.id.video_title_3);
@@ -52,12 +73,14 @@ public class RecyclerviewClickEvent /**implements View.OnTouchListener*/ {
         Button button = (Button) dialog.findViewById(R.id.like_btn);
         WebView videoView_3 = (WebView) dialog.findViewById(R.id.video_view_3);
         ImageView titleimage = (ImageView) dialog.findViewById(R.id.itemtitle_image);
+        main.setOverScrollMode(View.OVER_SCROLL_IF_CONTENT_SCROLLS);
         titleimage.setImageDrawable(titlecategory(ldItem.getLd_Category()));
         String result = ldItem.getLd_ImageUrl().replaceAll("\\/","/");
-        Picasso.with(context).load(ConAdapter.SERVER_URL + result).resize(1500,2420).into(imgViewIcon);
+        Picasso.with(context).load(ConAdapter.SERVER_URL + result).into(imgViewIcon);
         txtViewTitle.setText(ldItem.getLd_Title());
         txtViewId.setText(ldItem.getLd_Id());
-        imgViewIcon.setFocusable(false);
+        imgViewIcon.setFocusable(true);
+        imgViewIcon.setOnTouchListener(this);
         imgViewFace.setFocusable(false);
         imgViewFace.getSettings().setJavaScriptEnabled(true);
         imgViewFace.getSettings().setDomStorageEnabled(true);
@@ -68,14 +91,12 @@ public class RecyclerviewClickEvent /**implements View.OnTouchListener*/ {
         videoView_2.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
         videoView_3.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
         if (Build.VERSION.SDK_INT >= 19) {
-            imgViewIcon.setLayerType(View.LAYER_TYPE_HARDWARE, null);
             imgViewFace.setLayerType(View.LAYER_TYPE_HARDWARE, null);
             videoView_1.setLayerType(View.LAYER_TYPE_HARDWARE, null);
             videoView_2.setLayerType(View.LAYER_TYPE_HARDWARE, null);
             videoView_3.setLayerType(View.LAYER_TYPE_HARDWARE, null);
         }
         else {
-            imgViewIcon.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
             imgViewFace.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
             videoView_1.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
             videoView_2.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
@@ -169,80 +190,54 @@ public class RecyclerviewClickEvent /**implements View.OnTouchListener*/ {
         }
         return drawable;
     }
-/**
+    @SuppressLint("ClickableViewAccessibility")
     @Override
-    public boolean onTouch(View view, MotionEvent ev) {
+    public boolean onTouch(View view, MotionEvent event) {
+        final int X = (int) event.getRawX();
+        final int Y = (int) event.getRawY();
+        width = imgViewIcon.getWidth();
+        height = imgViewIcon.getHeight();
 
-        // Let the ScaleGestureDetector inspect all events.
-        mScaleDetector.onTouchEvent(ev);
+        scaleWidth = width/(float) X;
+        scaleHeight = height/(float) Y;
 
-        final int action = MotionEventCompat.getActionMasked(ev);
-
-        switch (action) {
-            case MotionEvent.ACTION_DOWN: {
-                final int pointerIndex = MotionEventCompat.getActionIndex(ev);
-                final float x = MotionEventCompat.getX(ev, pointerIndex);
-                final float y = MotionEventCompat.getY(ev, pointerIndex);
-
-                // Remember where we started (for dragging)
-                mLastTouchX = x;
-                mLastTouchY = y;
-                // Save the ID of this pointer (for dragging)
-                mActivePointerId = MotionEventCompat.getPointerId(ev, 0);
+        // createa matrix for the manipulation
+        matrix = new Matrix();
+        ScaleLayoutHelper.ScaleLayoutParams lParams = (ScaleLayoutHelper.ScaleLayoutParams) view.getLayoutParams();
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                end = false;
                 break;
-            }
-
-            case MotionEvent.ACTION_MOVE: {
-                // Find the index of the active pointer and fetch its position
-                final int pointerIndex =
-                        MotionEventCompat.findPointerIndex(ev, mActivePointerId);
-
-                final float x = MotionEventCompat.getX(ev, pointerIndex);
-                final float y = MotionEventCompat.getY(ev, pointerIndex);
-
-                // Calculate the distance moved
-                final float dx = x - mLastTouchX;
-                final float dy = y - mLastTouchY;
-
-                mPosX += dx;
-                mPosY += dy;
-
-                invalidate();
-
-                // Remember this touch position for the next move event
-                mLastTouchX = x;
-                mLastTouchY = y;
-
+            case MotionEvent.ACTION_UP:
+                end = false;
                 break;
-            }
-
-            case MotionEvent.ACTION_UP: {
-                mActivePointerId = INVALID_POINTER_ID;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                _xDelta = X;
+                _yDelta = Y;
+                end = true;
+                Log.d("Down_scale", String.valueOf(X) + "*" + String.valueOf(Y));
                 break;
-            }
-
-            case MotionEvent.ACTION_CANCEL: {
-                mActivePointerId = INVALID_POINTER_ID;
+            case MotionEvent.ACTION_POINTER_UP:
+                end = false;
                 break;
-            }
-
-            case MotionEvent.ACTION_POINTER_UP: {
-
-                final int pointerIndex = MotionEventCompat.getActionIndex(ev);
-                final int pointerId = MotionEventCompat.getPointerId(ev, pointerIndex);
-
-                if (pointerId == mActivePointerId) {
-                    // This was our active pointer going up. Choose a new
-                    // active pointer and adjust accordingly.
-                    final int newPointerIndex = pointerIndex == 0 ? 1 : 0;
-                    mLastTouchX = MotionEventCompat.getX(ev, newPointerIndex);
-                    mLastTouchY = MotionEventCompat.getY(ev, newPointerIndex);
-                    mActivePointerId = MotionEventCompat.getPointerId(ev, newPointerIndex);
+            case MotionEvent.ACTION_MOVE:
+                if(end){
+                    handler = new Handler();
+                    final Runnable r = new Runnable() {
+                        public void run() {
+                            // resize the bit map
+                            imgViewIcon.setScaleY(scaleHeight);
+                            imgViewIcon.setScaleX(scaleWidth);
+                            handler.postDelayed(this, 1000);
+                        }
+                    };
+                    r.run();
+                Log.d("Move_scale", String.valueOf( X + _xDelta) + "*" + String.valueOf(Y + _yDelta));
                 }
+
                 break;
-            }
         }
+        imgViewIcon.invalidate();
         return true;
     }
- */
 }
